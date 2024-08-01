430d72a1b740a956a1c0dd52b7af56dd86415cbc
MarcusO17 <marcusonggeye@gmail.com>
2024-08-01 14:50:08 +0800

    added basic rag-prompt for all models

--- components.py
+++ components.py
@@ -8,9 +8,10 @@ import chromadb
 class Components:
     load_dotenv()
 
-    def __init__(self,embed,reranker):
+    def __init__(self,embed,reranker,model_name):
         self.embed = embed
         self.reranker = reranker
+        self.model_name = model_name
 
     def get_embedding_model(self):
         embedding_model = HuggingFaceEmbedding(model_name = self.embed)
@@ -27,8 +28,54 @@ class Components:
         chroma_collection = db.get_or_create_collection("temp")
         return chroma_collection
     
-    def get_groq_llm(self,model_name):
-        return Groq(model_name,api_key=os.getenv("GROQ_API_KEY"))
+    def get_groq_llm(self):
+        return Groq(self.model_name,api_key=os.getenv("GROQ_API_KEY"))
+    
+    def get_qa_template(self):
+        if self.model_name == "llama-3.1-70b-versatile":
+            return """You are an assistant for question-answering tasks. 
+            Use the following pieces of retrieved context to answer the question.
+            If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
+            Question: {question} 
+            Context: {context} 
+            Answer:"""
+        elif self.model_name == "llama-3.1-8b-instant":
+            return """You are an assistant for question-answering tasks. 
+            Use the following pieces of retrieved context to answer the question.
+            If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
+            Question: {question} 
+            Context: {context} 
+            Answer:"""
+        elif self.model_name == " llama3-70b-8192":
+            return """You are an assistant for question-answering tasks. 
+            Use the following pieces of retrieved context to answer the question.
+            If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
+            Question: {question} 
+            Context: {context} 
+            Answer:"""
+        elif self.model_name == "llama3-8b-8192":
+            return """You are an assistant for question-answering tasks. 
+            Use the following pieces of retrieved context to answer the question.
+            If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
+            Question: {question} 
+            Context: {context} 
+            Answer:"""
+        elif self.model_name == "mixtral-8x7b-32768":
+            return """You are an assistant for question-answering tasks. 
+            Use the following pieces of retrieved context to answer the question.
+            If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
+            Question: {question} 
+            Context: {context} 
+            Answer:"""
+        elif self.model_name == "gemma2-9b-it":
+            return """You are an assistant for question-answering tasks. 
+            Use the following pieces of retrieved context to answer the question.
+            If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
+            Question: {question} 
+            Context: {context} 
+            Answer:"""
+
+    
     
     """TODO
     CUSTOM QA Templates for Each Model
