0df2c1748875dff12b4de6c12327f77d86d07ab0
MarcusO17 <marcusonggeye@gmail.com>
2024-08-08 12:26:46 +0800

    init branch for custom groq llm

---
+++ groqllm.py
@@ -0,0 +1,61 @@
+from typing import Optional, List, Mapping, Any
+import os
+
+from groq import Groq
+from llama_index.core import SimpleDirectoryReader, SummaryIndex
+from llama_index.core.callbacks import CallbackManager
+from llama_index.core.llms import (
+    CustomLLM,
+    CompletionResponse,
+    CompletionResponseGen,
+    LLMMetadata,
+)
+from llama_index.core.llms.callbacks import llm_completion_callback
+from llama_index.core import Settings
+
+
+class GroqLLM(CustomLLM):
+    context_window: int = 3900
+    num_output: int = 256
+    model_name: str = "custom"
+    dummy_response: str = "My response"
+    
+    def __init__(self) -> None:
+        self.client = Groq(
+            api_key=os.environ.get("GROQ_API_KEY"),
+        )
+            
+    @property
+    def metadata(self) -> LLMMetadata:
+        """Get LLM metadata."""
+        return LLMMetadata(
+            context_window=self.context_window,
+            num_output=self.num_output,
+            model_name=self.model_name,
+        )
+
+    @llm_completion_callback()
+    def complete(self, prompt: str, **kwargs: Any) -> CompletionResponse:
+        chat_completion = self.client.chat.completions.create(
+            messages=[
+                {
+                    "role": "system",
+                    "content": "you are a helpful assistant."
+                },
+                {
+                    "role": "user",
+                    "content": "Explain the importance of fast language models",
+                }
+            ],
+            model=self.model_name,
+        )
+        return CompletionResponse(text=chat_completion.choices[0].message.content)
+
+    @llm_completion_callback()
+    def stream_complete(
+        self, prompt: str, **kwargs: Any
+    ) -> CompletionResponseGen:
+        response = ""
+        for token in self.dummy_response:
+            response += token
+            yield CompletionResponse(text=response, delta=token)
