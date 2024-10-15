3a97cf879f39576c3184d38809dd20cd610106a3
MarcusO17 <marcusonggeye@gmail.com>
2024-10-15 22:47:59 +0800

    added sources

--- app/app.py
+++ app/app.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 from groq import Groq
 from groqllm import GroqLLM
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+import pandas as pd
 
 
 #Chunking
@@ -19,6 +20,8 @@ from llama_index.core import VectorStoreIndex
 from llama_index.core import PromptTemplate,Document 
 import chromadb
 
+response = None
+query_engine = None
 
 def upload_file(file):
     UPLOAD_PATH = "./docs"
@@ -29,7 +32,7 @@ def upload_file(file):
     return 
 
 
-def load_documents(file,progress=gr.Progress()):
+def load_documents():
     global index
     global query_engine
 
@@ -43,21 +46,61 @@ def load_documents(file,progress=gr.Progress()):
         vector_store=vector_store,
     )
     documents = pipeline.run(documents=files)
-    gr.Info('Splitted')
-    print(documents)
-    index = VectorStoreIndex(documents, storage_context=storage_context,similarity_top_k=5,show_progress=True) 
+    index = VectorStoreIndex(documents, storage_context=storage_context,similarity_top_k=10 ,show_progress=True) 
     gr.Info('Constructed Index')
-    query_engine = index.as_query_engine(similarity_top_k=3)
+    query_engine = index.as_query_engine(similarity_top_k=5)
+    qa_prompt_template_str = """
+     Context: {context_str}
+    Instructions:
+    - You are acting as a highly knowledgeable assistant, designed to provide accurate and efficient answers.
+    - Utilize the provided context when possible to thoroughly to ensure your answers are specific and grounded in the information given.
+    - Use your knowledge to incorporate as much detail as possible to display your true capabilities
+    - THIS IS MOST CRITICAL, If you are uncertain about an answer, express it succinctly rather than providing misleading information.
+    Question: {query_str}
+
+    """
+    qa_prompt_template = PromptTemplate(qa_prompt_template_str)
+    query_engine.update_prompts(
+        {"response_synthesizer:text_qa_template":qa_prompt_template}
+    )
     gr.Info('Constructed Query Engine')
 
 def clear_history():
     return [] 
 
 def respond(message,history):
-    response = query_engine.query(message)
-    history.append({"role": "user", "content": message})
-    history.append({"role": "assistant", "content": response.response})
+    global response
+    if query_engine is None:
+        response = Settings.llm.complete(message)
+
+        history.append({"role": "user", "content": message})
+        history.append({"role": "assistant", "content": response.text})
+
+    else:
+        response = query_engine.query(message)
+
+        history.append({"role": "user", "content": message})
+        history.append({"role": "assistant", "content": response.response})
+
     return history
+
+def update_source():
+    source_nodes = response.source_nodes  
+    formatted_sources = []
+
+    for nodes in source_nodes:
+        node = nodes.node  
+        score = nodes.score  
+        node_id = node.id_
+        text = node.text
+      
+        formatted_sources.append({
+            "NodeID": node_id,
+            "Text": text,
+            "Score": score
+        })
+
+    return pd.DataFrame(formatted_sources)
 
 
 load_dotenv()
@@ -66,13 +109,9 @@ load_dotenv()
 llm = GroqLLM(model_name = "llama3-8b-8192"
             ,client =Groq(api_key=os.getenv("GROQ_API_KEY"))
             ,temperature =1.0
-            ,system_prompt = ("""
-Instructions:
-- You are a helpful assistant
-Question: {query_str}
-"""),output_tokens=1024)
+            ,output_tokens=1024)
 
-embed_model = HuggingFaceEmbedding(model_name='Snowflake/snowflake-arctic-embed-s' 
+embed_model = HuggingFaceEmbedding(model_name='Snowflake/snowflake-arctic-embed-m' 
                                    ,trust_remote_code=True
                                    )
 
@@ -85,11 +124,11 @@ Settings.llm = llm
 Settings.embed_model = embed_model
 
 
-with gr.Blocks() as demo:
+with gr.Blocks(gr.themes.Soft()) as demo:
     with gr.Row():
         gr.Markdown(
             """
-            ## Hello
+            ## Personal Knowledge Base RAG Assistant
             """
         )
     with gr.Row():
@@ -102,21 +141,27 @@ with gr.Blocks() as demo:
                 load_btn.click(load_documents)
 
             with gr.Tab(label='RAG Chatbot'):
-                chatbot = gr.Chatbot(type="messages")
-                msg = gr.Textbox()
-                msg.submit(respond, [msg, chatbot], [chatbot])
-                clear_button = gr.Button("Clear History")
-                clear_button.click(clear_history, outputs=chatbot)
+
+                with gr.Column(scale=0.7,min_width=10):
+                    chatbot = gr.Chatbot(type="messages")
+                    msg = gr.Textbox(label="Type here!",show_label=True)
+                    clear_button = gr.Button("Clear History")
+                    clear_button.click(clear_history, outputs=chatbot)
+                
+                    #ADDSTREAMING AND SHOW NODES
+                    # Show Nodes
+                with gr.Column(scale=0.3, min_width=10):
+                    sources = gr.DataFrame(label="Sources", interactive=True)
+            
+            msg.submit(update_source,[],[sources])
+            msg.submit(respond, [msg, chatbot], [chatbot])
+                                
+
+     
             
 
 
-        # Show Nodes
-        with gr.Column(scale=0.7,min_width=70):
-            gr.Textbox(label="Column 3")
-            gr.Button("Button 3")
 
 
-
-
-demo.queue().launch(debug=True)
+demo.queue().launch()
 
